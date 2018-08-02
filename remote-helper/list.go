e0b9036f46aa3bfec5115ec600d15d4dd5f0881e
bryn austin bellomy <bryn.bellomy@gmail.com>
2018-08-02 03:14:22 -0500

    remote-helper: Fix fetch error caused by reversed refName/commit pair

--- remote-helper/list.go
+++ remote-helper/list.go
@@ -11,7 +11,7 @@ func getRefs() ([]string, error) {
 	}
 	refsList := make([]string, 0)
 	for _, ref := range refs {
-		refsList = append(refsList, fmt.Sprintf("%s %s", ref.Commit, ref.Name))
+		refsList = append(refsList, fmt.Sprintf("%s %s", ref.Name, ref.Commit))
 	}
 	refsList = append(refsList, "@refs/heads/master HEAD")
 	return refsList, nil
