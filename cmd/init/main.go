b6e66b4e0d27189c887e011921a0d11f39473455
bryn austin bellomy <bryn.bellomy@gmail.com>
2018-08-02 00:17:32 -0500

    Adding conscience-init command (WIP)

---
+++ cmd/init/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"../../repo"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("usage: conscience-init <repo id>")
+	}
+
+	repoID := os.Args[1]
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := repo.Open(cwd)
+	if err != nil {
+		panic(err)
+	}
+
+	err = r.SetupConfig(repoID)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = n.eth.CreateRepository(ctx, repoID)
+	if err != nil {
+		return err
+	}
+
+	head, err := repo.Head()
+	if err != nil {
+		return err
+	}
+
+	_, err = n.eth.UpdateRef(ctx, repoID, "refs/heads/master", head.Hash().String())
+	if err != nil {
+		return err
+	}
+}
