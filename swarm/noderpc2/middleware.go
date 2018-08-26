19971b988b9b665f506dc8be73b611af05bf98a6
bryn austin bellomy <bryn.bellomy@gmail.com>
2018-08-25 22:32:53 -0500

    swarm: more useful RPC server logging

--- swarm/noderpc2/middleware.go
+++ swarm/noderpc2/middleware.go
@@ -8,17 +8,22 @@ import (
 
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		log.Infof("[rpc server] req ~> %+v", req)
-		log.Infof("[rpc server] info ~> %+v", info)
-		return handler(ctx, req)
+		log.Debugf("[rpc server] %v %+v", info.FullMethod, req)
+		x, err := handler(ctx, req)
+		if err != nil {
+			log.Errorf("[rpc server] %v", err)
+		}
+		return x, err
 	}
 }
 
 func StreamServerInterceptor() grpc.StreamServerInterceptor {
 	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		log.Infof("[rpc server] srv ~> %+v", srv)
-		log.Infof("[rpc server] info ~> %+v", info)
+		log.Debugf("[rpc server] %v", info.FullMethod)
 		err := handler(srv, stream)
+		if err != nil {
+			log.Errorf("[rpc server] %v", err)
+		}
 		return err
 	}
 }
