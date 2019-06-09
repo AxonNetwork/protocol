1dc080aa340ed0c840799f421aa36025ff5f323d
Bryn Bellomy <bryn.bellomy@gmail.com>
2019-06-09 15:35:58 -0500

    eth: update truffle

--- eth/truffle.js
+++ eth/truffle.js
@@ -1,83 +1,101 @@
-var HDWalletProvider = require('truffle-hdwallet-provider')
+const HDWalletProvider = require('truffle-hdwallet-provider')
 
 // defaults
-var conf = {
-  mnemonic: "",
-  host: ""
-}
-var appCfg = require('rc')('axon', conf);
-var mnemonic = appCfg.node.EthereumBIP39Seed
-var host = appCfg.node.EthereumHost
+// const conf = {
+//     mnemonic: '',
+//     host: ''
+// }
+// const appCfg = require('rc')('axon', conf)
+// const mnemonic = appCfg.node.EthereumBIP39Seed
+// const host = appCfg.node.EthereumHost
 
 module.exports = {
-  rpc: {
-    host: 'localhost',
-    port: 8545,
-    gas: 4000000,
-    // gas: 4000000,
-  },
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
   networks: {
-    dev: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*",
-      gas: 4000000,
-      // from: '0xCE1f819Af3447C59676bc03B119721AbCD40EFBE',
-    },
-    localgeth: {
-      network_id: 23332,
-      provider: function() {
-          return new HDWalletProvider(mnemonic, host)
-      },
-      gas: 4000000,
-      gasPrice: 50000000000,
-    },
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    // development: {
+    //  host: "127.0.0.1",     // Localhost (default: none)
+    //  port: 8545,            // Standard Ethereum port (default: none)
+    //  network_id: "*",       // Any network (default: none)
+    // },
+
+    // Another network with more advanced options...
+    // advanced: {
+      // port: 8777,             // Custom port
+      // network_id: 1342,       // Custom network
+      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+      // from: <address>,        // Account to send txs from (default: accounts[0])
+      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // },
+
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
     kaleido: {
-      provider: function() {
-          return new HDWalletProvider(mnemonic, `https://${process.env.KALEIDO_USERNAME}:${process.env.KALEIDO_PASSWORD}@${KALEIDO_ETH_HOST_HTTPS}`)
-      },
-      gas: 6900000,
-      gasPrice: 50000000000,
+        provider: () => new HDWalletProvider(process.env.DEPLOYMENT_MNEMONIC, `https://${process.env.KALEIDO_USERNAME}:${process.env.KALEIDO_PASSWORD}@${process.env.KALEIDO_ETH_HOST_HTTPS}`),
+        network_id: '*',
+        gas: 8000000,
+        gasPrice: 0,
+        type: 'quorum',
+        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     awsgeth: {
       network_id: 23332,
-      provider: function() {
-          return new HDWalletProvider(mnemonic, host)
-      },
+      provider: () => new HDWalletProvider(mnemonic, host),
       gas: 6900000,
       gasPrice: 50000000000,
-    },
-    mainnet: {
-      network_id: 1,
-      provider: function() {
-          return new HDWalletProvider('12 words go here', 'https://mainnet.infura.io/API KEY HERE')
-      },
-      gas: 4000000,
-      gasPrice: 50000000000,
-    },
-    mainnetgeth: {
-      host: "localhost",
-      port: 8545,
-      network_id: 1,
-      gas: 4000000,
-      gasPrice: 50000000000,
-    },
-    ropsten: {
-      port: 8545,
-      network_id: 3,
-      provider: function() {
-          return new HDWalletProvider('12 words go here', 'https://ropsten.infura.io/API KEY HERE')
-      },
-      gas: 4000000,
     },
     rinkeby: {
       port: 8545,
       network_id: 4,
-      provider: function() {
-          return new HDWalletProvider(mnemonic, host)
-      },
+      provider: () => new HDWalletProvider(mnemonic, host),
       gas: 4000000,
     },
+
+    // Useful for private networks
+    // private: {
+      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+      // network_id: 2111,   // This network is yours, in the cloud.
+      // production: true    // Treats this network as if it was a public net. (default: false)
+    // }
   },
-  migrations_directory: './migrations'
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
 }
+
+
+
